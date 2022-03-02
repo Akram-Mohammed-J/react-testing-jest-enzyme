1b212d8cf4389d8b5dc3ff6704436b97feeb8068
Apple <akram@melloon.com>
2022-03-02 13:37:09 +0530

    intial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/*
+ Configuring the enzyme with new instance 
+ Enzyme adapter for keep track of react componenets
+ */
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without crashing and non-empty component", () => {
+  /*Shallow is method that reenders the components 
+  one level deep
+  */
+  const wrapper = shallow(<App />);
+  /*debug is the method we used to see the 
+  skeleton of the component we are shallow 
+  rendering
+  */
+  console.log(wrapper.debug());
+  /*
+  Jest assertions are a basically the output we expect
+   from the test we are run
+  for ex:I Expect App Component to be non Empty 
+*/
+
+  /*
+exist() which check the component have nodes and return boolean
+toBe () which where we pass the value what we are expecting
+ */
+  expect(wrapper.exists()).toBe(true);
 });
